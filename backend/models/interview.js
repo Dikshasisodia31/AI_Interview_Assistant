8f50c75ec1be5dc83fb097f9652f502f147b88b5
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-07-05 12:53:42 +0530

    Setup routes and models

--- backend/models/interview.js
+++ backend/models/interview.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const interviewSchema = new mongoose.Schema(
     {
-        userId : {
+        user_id : {
             type:mongoose.Schema.Types.ObjectId,
             ref : "User",
         },
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {Register,Login} = require("../controllers/authController")
 
-
 router.get("/",(req,res)=>{
     res.json({
         message : "Auth route is working",
