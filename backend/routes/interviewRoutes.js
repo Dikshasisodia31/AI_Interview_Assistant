132d4bfa6aed1cfedaf716cde9202ba1ffc14157
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-07-06 20:29:41 +0530

    Add a route

--- backend/routes/interviewRoutes.js
+++ backend/routes/interviewRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {createInterview} = require("../controllers/InterviewController");
+const {getInterviewById} = require("../controllers/InterviewController");
 const {generateInterviewQuestions} = require("../controllers/InterviewController");
 
+router.get("/:id",getInterviewById);
 router.post("/",createInterview);
 router.post("/:id/generate",generateInterviewQuestions);
 
