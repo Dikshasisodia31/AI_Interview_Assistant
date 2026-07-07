ecfc0f89add2dd3b00bc5824d08f01cf5e843a5a
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-07-08 00:20:55 +0530

    Implement session API

--- ai-interview-coach/src/pages/Interview/InterviewSession.jsx
+++ ai-interview-coach/src/pages/Interview/InterviewSession.jsx
@@ -1,80 +1,126 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react';
-import { sampleQuestions } from '../../data/questions';
-import { useEffect } from 'react';
-import { Navigate , useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate,useParams } from 'react-router-dom';
+import api from '../../services/api';
 
 const InterviewSession = () => {
-//   const Navigate = useNavigate();
-//   const [answer,setAnswer] = useState(" ");
-//   const [questions,setQuestions] = useState();
-//   const [currentQuestion,setCurrentQuestion] = useState(0);
-//   const [loading,setLoading] = useState(true);
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-//   useEffect(()=>{
-//     fetchQuestions();
-//   }, []);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [interview, setInterview] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-//   const fetchQuestions = async () =>{
-//     try{
-//         const response = await generateQuestions(id);
-        
-//         setQuestions(response.data.questions);
-//     }catch(err){
-//        console.log(err);
-//     }finally{
-//         setLoading(false);
-//     }
-//   };
+    useEffect(() => {
+        fetchInterview();
+    }, []);
 
-//   const handleSubmit = () =>{
-//      console.log(answer);
+    const fetchInterview = async () => {
+        try {
+            const response = await api.get(`interviews/${id}`);
+            setInterview(response.data.interview);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-//      setAnswer("");
+    if (loading) {
+        return <h3 className='text-center'>Loading....</h3>
+    }
 
-//      if(currentQuestion < questions.length-1){
-//         setCurrentQuestion(currentQuestion+1);
-//      }else{
-//         Navigate("/interview/result");
-//      }
-//   };
-  return (
-    <div className="container">
-        {/* <div className="row justify-content-center">
-           
-            <div className="col">
-                <div className="card text-center mt-5 shadow border-0">
-                    <div className="card-body p-5">
-                        <div>
-                        <span className='head1 badge'>
-                            Question {currentQuestion+1}/5;
-                        </span>
+    if (!interview) {
+        return <h3 className='text-center'>Interview not found.</h3>
+    }
+
+    const question = interview.questions[currentQuestion];
+
+    const handleAnswerChange = (e) => {
+        console.log(e.target.value);
+
+        const updatedInterview = { ...interview };
+
+        updatedInterview.questions[currentQuestion].answer = e.target.value;
+
+        setInterview(updatedInterview);
+    };
+
+    const handlePrev = () => {
+        if (currentQuestion > 0) {
+            setCurrentQuestion(currentQuestion - 1);
+        }
+    };
+
+    const handleNext = () => {
+        if (currentQuestion < interview.questions.length - 1) {
+            setCurrentQuestion(currentQuestion + 1);
+        }
+    };
+
+
+    const handleSubmitInterview = async () => {
+        try {
+            //   await api.put(`/interviews/${id}/submit`,{
+            //     questions : interview.questions,
+            //   })
+            navigate(`/interview/result/${id}`);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col">
+                    <div className="card text-center mt-5 shadow border-0">
+                        <div className="card-body p-5">
+                            <div>
+                                <span className='head1 badge'>
+                                    Question {currentQuestion + 1} of {interview.questions.length};
+                                </span>
+                            </div>
+                            <h2 className='mb-4'>
+                                {question.question}
+                            </h2>
+                            <div>
+                                <label className='form-label'>
+                                    Your answer.
+                                </label>
+                                <textarea className='form-control'
+                                    placeholder="Write your answer here..."
+                                    value={question.answer}
+                                    onChange={handleAnswerChange}
+                                />
+                            </div>
+                            <button disabled={currentQuestion == 0}
+                                onClick={handlePrev}
+                                className='btns mt-3'
+                            >Previous
+                            </button>
+
+                            {currentQuestion === interview.questions.length - 1 ? (
+                                <button
+                                    className='btns btn btn-outline-light mt-4'
+                                    onClick={handleSubmitInterview}>
+                                    Submit Interview
+                                </button>
+                            ) : (
+                                <button
+                                    disabled={currentQuestion == interview.questions.length - 1}
+                                    onClick={handleNext}
+                                    className='btns'
+                                    >
+                                    Next
+                                </button>
+                            )}
                         </div>
-                        <h2 className='mb-4'>
-                            {questions[currentQuestion]}
-                        </h2>
-                        <div>
-                            <label className='form-label'>
-                               Your answer.
-                            </label>
-                            <textarea className='form-control'
-                            placeholder="Write your answer here..."
-                            value={answer}
-                            onChange={(e)=>
-                                setAnswer(e.target.value)
-                            }
-                            />     
-                        </div>
-                        <button className='btns btn btn-outline-light mt-4' onClick={handleSubmit}>Submit Answer</button>
                     </div>
                 </div>
             </div>
-        </div> */}
-        <h1> Hii</h1>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default InterviewSession
