03925774ffd49fc6b5f02648a9c44a008f44cb5e
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-08-19 00:22:59 +0530

    Solve production issue

--- ai-interview-coach/src/services/api.js
+++ ai-interview-coach/src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL : "http://localhost:3000/api",
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL : "https://ai-interview-assistant-5qwq.onrender.com/api",
+
+    //baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
